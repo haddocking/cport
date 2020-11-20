f4ef27c4646cb2f9ca3070d12e5c1d7b20c7501f
Andreas <palazisa93@gmail.com>
2020-11-20 15:07:51 +0100

    Reformed multiprocessing, added printing functions and common printing for parallel processes

--- cport.py
+++ cport.py
@@ -10,50 +10,77 @@ import argparse
 from tools import threshold
 from tools import calc_sasa
 from tools import residiues_distance, predictors
-from tools import reconstract_pdb
+from tools import reconstruct_pdb
 from tools import save_csv
 from tools import validations
 import os
 import random
+import time
 
 
-def func1(i, re,params,main_dir):
-    print("s1")
-    re[i] = pro_mate.run(params, main_dir)
-    print("e1")
+def func1(i, predictors_dic, params, main_dir):
+    predictors_dic[i] = pro_mate.run(params, main_dir)
 
-def func2(i, re,params,main_dir):
-    print("s2")
-    re[i] = meta_ppisp.run(params, main_dir)
-    print("e2")
 
-def func3(i, re,params,main_dir):
-    print("s3")
-    re[i] = spider.run(params, main_dir)
-    print("e3")
+def func2(i, predictors_dic, params, main_dir):
+    predictors_dic[i] = meta_ppisp.run(params, main_dir)
+
+
+def func3(i, predictors_dic, params, main_dir):
+    predictors_dic[i] = spider.run(params, main_dir)
+
+
+def get_processes(web_servers, predictors_dic, params, main_dir):
+    processes = []
+    for i, server in enumerate(web_servers):
+        if server == "promate":
+            p = Process(target=func1, args=(i, predictors_dic, params, main_dir))
+        elif server == "meta_ppisp":
+            p = Process(target=func2, args=(i, predictors_dic, params, main_dir))
+        elif server == "spidder":
+            p = Process(target=func3, args=(i, predictors_dic, params, main_dir))
+        else:
+            raise AssertionError("Webserver not in the list")
+        processes.append(p)
+    return processes
+
+
+def get_multiprocess_string(web_servers, main_dir):
+    temp_dir = os.path.join(main_dir, "temp")
+    print_string = ""
+    for server in web_servers:
+        sfile = server + ".status"
+        temp_file = os.path.join(temp_dir, sfile)
+        open_file = open(temp_file, "r")
+        line = open_file.readlines()[-1]
+        print_string = print_string + line
+        open_file.close()
+    return print_string
+
 
 def run(params, main_dir):
-
     manager = multiprocessing.Manager()
     predictors_dic = manager.dict()
+    web_servers = ["promate", "meta_ppisp", "spidder"]
+    #web_servers = ["promate"]
+    print("Preparing the {} web servers to run in parallel".format(len(web_servers)))
+    processes = get_processes(web_servers, predictors_dic, params, main_dir)
 
-    p1 = Process(target=func1, args=(0, predictors_dic,params,main_dir))
-    p1.start()
-    p2 = Process(target=func2, args=(1, predictors_dic,params,main_dir))
-    p2.start()
-    p3 = Process(target=func3, args=(2, predictors_dic,params,main_dir))
-    p3.start()
-    p1.join()
-    p2.join()
-    p3.join()
+    print("Parallel run is starting")
+    for p in processes:
+        p.start()
 
+    while any([p.is_alive() for p in processes]):
+        time.sleep(5)
+        print_string = get_multiprocess_string(web_servers, main_dir)
+        print(print_string, end="\n")
     return predictors_dic
 
 
-def get_unique_folder(tools_dir):
+def get_unique_folder(tools_folder):
     while True:
         random_int = random.randint(1, 10000)
-        main_dir = os.path.join(tools_dir, "run_{}".format(random_int))
+        main_dir = os.path.join(tools_folder, "run_{}".format(random_int))
         if not os.path.exists(main_dir):
             return main_dir
 
@@ -75,12 +102,15 @@ if __name__ == "__main__":
             it will automatically use 3: very sensitive (recommended from the Cport webserver)
             """
             # I am not sure what to do if you don't set a chain id
-            arg_parser.add_argument('-chain_id', help="optional argument",dest="chain_id")
-            arg_parser.add_argument('-threshold', help="optional preset to 3", type=int, default=3,dest="threshold")
-            arg_parser.add_argument('-pdb_id',help="give the id of the pdb XXXX",dest="pdb_id")
-            arg_parser.add_argument('-pdb_file',help="load the pdb from a local file",type=validations.pdb_assertions,dest="pdb_file")
-            arg_parser.add_argument('-sequence_file', help="load the seq from a local file",type=validations.seq_assertions,dest="seq_file")
-            arg_parser.add_argument('-alignment_format',help="give the format of the alignment",choices=["FASTA","CLUSTAL","MAF","PHYLIP"],dest="al")
+            arg_parser.add_argument('-chain_id', help="optional argument", dest="chain_id")
+            arg_parser.add_argument('-threshold', help="optional preset to 3", type=int, default=3, dest="threshold")
+            arg_parser.add_argument('-pdb_id', help="give the id of the pdb XXXX", dest="pdb_id")
+            arg_parser.add_argument('-pdb_file', help="load the pdb from a local file", type=validations.pdb_assertions,
+                                    dest="pdb_file")
+            arg_parser.add_argument('-sequence_file', help="load the seq from a local file",
+                                    type=validations.seq_assertions, dest="seq_file")
+            arg_parser.add_argument('-alignment_format', help="give the format of the alignment",
+                                    choices=["FASTA", "CLUSTAL", "MAF", "PHYLIP"], dest="al")
             cl_arguments = arg_parser.parse_args()
             validations.argument_assertions(cl_arguments)
         else:
@@ -96,7 +126,6 @@ if __name__ == "__main__":
         The object will be the same if you choose ID or FILE.
         pdb_file, sequence_file, alignment_format, chain_id, threshold"""
         input_params = parser.run(cl_arguments, run_dir)
-
         """
         This is a hard-coded part of the script. 
         I use it to save time when I already have the files from the webservers.
@@ -139,12 +168,14 @@ if __name__ == "__main__":
         name : The name of the webserver
         """
         web_results = run(input_params, main_dir=run_dir)
-
+        print("Number of webservers: {}".format(len(web_results.values())))
+        print("Successful predictors: {}".format(sum([i.success for i in web_results.values()])))
         """
         Identifies if the predictor ran successfully.
         Set up the thresholds based on the successful predictors and the input of the user.
         Updates the cutoff_rank and cutoff_score of the predictor Objects
         """
+        print("Update the threshold values based on the successful predictors")
         predictors_list = threshold.run(web_results.values(), tools_dir, run_dir)
 
         """
@@ -152,10 +183,11 @@ if __name__ == "__main__":
         (You must install freesasa) 
         I copied the script from WHISCY
         Return a list of  residue numbers
-        I filter the residues based on the value of relative mainchain and relative sidechain 
+        I filter the residues based on the value of relative main chain and relative side chain 
         Both values must be greater or equal to 15 (I copied the value from Cport)
         In some cases freesasa return N/A I am not sure what I should do, for now I remove these residue
         """
+        print("Calculate solvent accessible surface area")
         surface = calc_sasa.run(input_params.pdb_file)
 
         """
@@ -165,6 +197,7 @@ if __name__ == "__main__":
         Returns a list of tuples.
         Each tuple contain two residue numbers and the distance between them
         """
+        print("Calculate the residues distance")
         distance_list = residiues_distance.run(input_params, tools_dir, run_dir)
 
         """
@@ -172,6 +205,7 @@ if __name__ == "__main__":
         I use various filters copied from Cport
         More details in predictors.py script
         """
+        print("Filter the residues based on precalculated residues and threshold")
         updated_predictors = predictors.update_res(predictor_list=predictors_list,
                                                    surface=surface,
                                                    distance_list=distance_list)
@@ -180,12 +214,14 @@ if __name__ == "__main__":
         I combine the active and passive residues from all the predictors
         I create a final PDB file with scores on the b-factor column
         """
-        reconstract_pdb.run(predictors_list=updated_predictors, main_dir=run_dir)
+        print("Construct the final PDB")
+        reconstruct_pdb.run(predictors_list=updated_predictors, main_dir=run_dir)
 
         """
         A table with all the residue numbers and predictors
         I mark the active residues with + and the passive residues with -
         """
+        print("Create the residues table")
         save_csv.run(updated_predictors, input_params.pdb_file, run_dir)
 
     except AssertionError as ae:
