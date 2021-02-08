daadfbe2d63d6baf6a56c0b26e95f3153f626f95
Andreas <palazisa93@gmail.com>
2021-02-08 18:26:56 +0100

    Haddock — freesasa implemented

--- tools/calc_sasa.py
+++ tools/calc_sasa.py
@@ -1,13 +1,13 @@
 import subprocess
 from Bio.PDB import PDBParser
 import freesasa
+from tools import haddock_calc_sasa
 
 
 class ResidueSASA():
-    def __init__(self, name, number, tot_rel, sd_rel, bk_rel):
+    def __init__(self, name, number, sd_rel, bk_rel):
         self.name = name
         self.number = number
-        self.relative_total = tot_rel
         self.relative_sidechain = sd_rel
         self.relative_mainchain = bk_rel
 
@@ -17,25 +17,20 @@ def parse_rsa_file(rsa_values):
     residue_sasas = []
     for res_id in rsa_values:
         res = rsa_values[res_id]
-        name =  res.residueType
-        number = int(res.residueNumber[:3])
+        name =  res['name']
+        number = int(res_id[:3])
         # Difference between NACCESS and freesasa output
         try:
-            relative_total = float(res.relativeTotal)
-        except ValueError:
-            relative_total = -99.9
-        try:
-            relative_sidechain = float(res.relativeSideChain)
+            relative_sidechain = float(res['side_chain_rel'])
         except ValueError:
             relative_sidechain = -99.9
         try:
-            relative_mainchain = float(res.relativeMainChain)
+            relative_mainchain = float(res['main_chain_rel'])
         except ValueError:
             relative_mainchain = -99.9
 
         residue_sasas.append(ResidueSASA(name,
                                          number,
-                                         relative_total,
                                          relative_sidechain,
                                          relative_mainchain))
 
@@ -43,15 +38,11 @@ def parse_rsa_file(rsa_values):
 
 
 def run(pdb_obj):
-    parser = PDBParser()
-    structure = parser.get_structure(pdb_obj.name, pdb_obj.pdb_dir)
-    result, sasa_classes = freesasa.calcBioPDB(structure)
-    rsa_values = result.residueAreas()[pdb_obj.chain_id]
-
-    res = parse_rsa_file(rsa_values)
+    rsa_values = haddock_calc_sasa.get_accessibility(pdb_obj.pdb_dir)
+    res = parse_rsa_file(rsa_values[pdb_obj.chain_id])
 
     surface = []
     for s in res:
-        if s.relative_mainchain >= 0.15 or s.relative_sidechain >= 0.15:
+        if s.relative_mainchain >= 15 or s.relative_sidechain >= 15:
             surface.append(s.number)
     return surface
