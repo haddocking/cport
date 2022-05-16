2e6aa1f768681205eed21fcdaf83ffb3600a200d
AldovdN <90687940+AldovdN@users.noreply.github.com>
2022-05-16 13:55:56 +0200

    Implemented Predict Protein predictor

--- src/cport/modules/predictprotein.py
+++ src/cport/modules/predictprotein.py
@@ -1,7 +1,14 @@
 import logging
+import time
+import sys
+import os
+import zipfile
+import glob
+import pandas as pd
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 
 from cport.url import PREDICTPROTEIN_URL
 from cport.modules import utils
@@ -19,6 +26,10 @@ class Predictprotein:
         self.chain_id = chain_id
         self.wait = WAIT_INTERVAL
         self.tries = NUM_RETRIES
+        self.test_result_html = "https://predictprotein.org/visual_results?req_id=$1$0xeBp0R9$LnGQjDhYiCNTRQqkzios7/"
+        self.test_path = (
+            r"/Users/aldovandennieuwendijk/Documents/CPORT/test_output_requests/"
+        )
 
     def submit(self):
         """Makes a submission to the PredictProtein server"""
@@ -34,7 +45,7 @@ class Predictprotein:
         button, but following to the result page is still proving elusive.
         """
 
-        driver = webdriver.Safari()
+        driver = webdriver.Chrome()
         driver.get(PREDICTPROTEIN_URL)
 
         # identifies the textarea
@@ -47,9 +58,90 @@ class Predictprotein:
             "a.btn.btn-submit.btn-primary.btn-large"
         ).click()
 
-        html = driver.page_source
+        time.sleep(5)
+
+        html = driver.current_url
 
         return html
+
+    def retrieve_prediction_link(self, url=None):
+        download_path = (
+            r"/Users/aldovandennieuwendijk/Documents/CPORT/test_output_requests/"
+        )
+        options = Options()
+        options.add_experimental_option(
+            "prefs",
+            {
+                "download.default_directory": download_path,
+                "profile.default_content_setting_values.automatic_downloads": 2,
+            },
+        )
+        driver = webdriver.Chrome(chrome_options=options)
+        driver.get(self.test_result_html)
+
+        time.sleep(5)
+
+        completed = False
+        while not completed:
+            # checks for the binding site button
+            if driver.find_element_by_xpath('//*[@id="binding"]/a') != None:
+                completed = True
+            else:
+                # still running, wait a bit
+                log.debug(f"Waiting for Predict Protein to finish... {self.tries}")
+                time.sleep(self.wait)
+                driver.refresh()
+                self.tries -= 1
+            if self.tries == 0:
+                # if tries is 0, then the server is not responding
+                log.error(f"Predict Protein server is not responding, url was {url}")
+                sys.exit()
+
+        driver.find_element_by_xpath('//*[@id="binding"]/a').send_keys(Keys.ENTER)
+        time.sleep(5)
+
+        driver.find_element_by_xpath('//*[@id="Binding"]/div[2]/div/ul/li/a').send_keys(
+            Keys.ENTER
+        )
+        time.sleep(5)
+
+        driver.find_element_by_xpath(
+            '//*[@id="Binding"]/div[2]/div/ul/li/ul/li[1]/a'
+        ).click()
+        time.sleep(5)
+
+        return download_path
+
+    def parse_prediction(self, path=None):
+        prediction_dict = {"active": [], "passive": []}
+
+        zip_file = glob.glob(f"{path}*.zip")[0]
+        with zipfile.ZipFile(zip_file, "r") as zip_ref:
+            zip_ref.extractall(path)
+
+        os.remove(zip_file)
+
+        final_predictions = pd.read_csv(
+            f"{path}query.prona",
+            skiprows=11,
+            usecols=[0, 3],
+            names=["Residue_Number", "Protein_Pred"],
+            delim_whitespace=True,
+        )
+
+        for row in final_predictions.itertuples():
+            if row.Protein_Pred == 1:  # uppercase denotes a predicted interaction
+                prediction_dict["active"].append(row[0])
+            elif row.Protein_Pred == 0:
+                prediction_dict["passive"].append(row[0])
+            else:
+                log.warning(
+                    f"There appears that residue {row} is either empty or unprocessable"
+                )
+
+        os.remove(f"{path}query.prona")
+
+        return prediction_dict
 
     def run(self, test=False):
         """Execute the PredictProtein prediction."""
@@ -57,7 +149,7 @@ class Predictprotein:
         log.info(f"Will try {self.tries} times waiting {self.wait}s between tries")
 
         submitted_url = self.submit()
-        # prediction_url = self.retrieve_prediction_link(url=submitted_url)
-        # self.prediction_dict = self.parse_prediction(url=prediction_url)
+        result_path = self.retrieve_prediction_link(submitted_url)
+        self.prediction_dict = self.parse_prediction(path=result_path)
 
-        return submitted_url
+        return self.prediction_dict
