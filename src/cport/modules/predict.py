260a8dd2d628ad2c293bbbc1ec2de2795f2ce9bc
Rodrigo V Honorato <r.vargashonorato@uu.nl>
2023-08-31 12:57:05 +0200

    refactor predict.py

--- src/cport/modules/predict.py
+++ src/cport/modules/predict.py
@@ -9,73 +9,75 @@ from tensorflow import keras
 model_path = "model/keras_classifier_scriber_ispred4_sppider_csm_potential_scannet/"
 
 
-class ModelPredict:
-    def read_pred(path):
-        with open(path, "r") as pred:
-            reader = csv.reader(pred)
-            pred_dict = {rows[0]: rows for rows in reader}
+# class ModelPredict:
+def read_pred(path):
+    with open(path, "r") as pred:
+        reader = csv.reader(pred)
+        pred_dict = {rows[0]: rows for rows in reader}
 
-        return pred_dict
+    return pred_dict
 
-    def format_predictions(pred_int_dict):
-        """
-        Format predictions to remove any non-numerical entry, keeps prediction scores.
 
-        Parameters
-        ----------
-        pred_ind_dict : dict
-            Dictionary containing the predictions.
+def format_predictions(pred_int_dict):
+    """
+    Format predictions to remove any non-numerical entry, keeps prediction scores.
 
-        Output
-        ------
-        pred_int_dict : dict
-            Dictionary containing the predictions in a binary format.
-        """
-        for pred in pred_int_dict:
-            temp_list = pred_int_dict[pred][1:]
-            pred_int_dict[pred] = temp_list
+    Parameters
+    ----------
+    pred_ind_dict : dict
+        Dictionary containing the predictions.
 
-        for pred in pred_int_dict:
-            if pred != "predictor":
-                key = 0
-                for entry in pred_int_dict[pred]:
-                    if entry == "P" or entry == "-" or entry == 0:
-                        pred_int_dict[pred][key] = 0
-                    if entry == "A":
-                        pred_int_dict[pred][key] = 1
-                    if entry == "AP":
-                        pred_int_dict[pred][key] = 0.5
-                    else:
-                        temp_pred = pred_int_dict[pred][key]
-                        pred_int_dict[pred][key] = float(temp_pred)
-                    key += 1
+    Output
+    ------
+    pred_int_dict : dict
+        Dictionary containing the predictions in a binary format.
+    """
+    for pred in pred_int_dict:
+        temp_list = pred_int_dict[pred][1:]
+        pred_int_dict[pred] = temp_list
 
-        return pred_int_dict
+    for pred in pred_int_dict:
+        if pred != "predictor":
+            key = 0
+            for entry in pred_int_dict[pred]:
+                if entry == "P" or entry == "-" or entry == 0:
+                    pred_int_dict[pred][key] = 0
+                if entry == "A":
+                    pred_int_dict[pred][key] = 1
+                if entry == "AP":
+                    pred_int_dict[pred][key] = 0.5
+                else:
+                    temp_pred = pred_int_dict[pred][key]
+                    pred_int_dict[pred][key] = float(temp_pred)
+                key += 1
 
-    def prediction(predictions, threshold=0.6):
-        """
-        Take the different results and create a final prediction.
+    return pred_int_dict
 
-        Parameters
-        ----------
-        predictions : dict
-            Predictions made by the different servers.
-        threshold : int
-            Value to use as a threshold, defaults to 0.8.
 
-        Returns
-        -------
-        results : list
-            List of the predictions for each residue of the PDB.
+def make_prediction(predictions, threshold=0.6):
+    """
+    Take the different results and create a final prediction.
 
-        """
-        pred_dict = ModelPredict.format_predictions(predictions)
-        predictor = pred_dict.pop("predictor")
-        pred = pd.DataFrame(pred_dict)
-        model = keras.models.load_model(model_path)
-        # FIX THE LAYOUT OF THE PREDICTION DICT
-        probabilities = model.predict(pred)
+    Parameters
+    ----------
+    predictions : dict
+        Predictions made by the different servers.
+    threshold : int
+        Value to use as a threshold, defaults to 0.8.
 
-        results = [1 if prob > threshold else 0 for prob in np.ravel(probabilities)]
+    Returns
+    -------
+    results : list
+        List of the predictions for each residue of the PDB.
 
-        return results, probabilities, predictor
+    """
+    pred_dict = format_predictions(predictions)
+    predictor = pred_dict.pop("predictor")
+    pred = pd.DataFrame(pred_dict)
+    model = keras.models.load_model(model_path)
+    # FIX THE LAYOUT OF THE PREDICTION DICT
+    probabilities = model.predict(pred)  # type: ignore
+
+    results = [1 if prob > threshold else 0 for prob in np.ravel(probabilities)]
+
+    return results, probabilities, predictor
