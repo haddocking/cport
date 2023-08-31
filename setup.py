e0df08067b4f89a43b322857ae0a963d969ba62c
Rodrigo V Honorato <r.vargashonorato@uu.nl>
2023-08-31 11:18:32 +0200

    update version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as f:
 setup(
     name="cport",
     license="Apache License 2.0",
-    version="0.1.0",
+    version="0.2.0-alpha",
     author="",
     description="",
     author_email="",
